cb859a43ac28cf3842f784f5103330d76e48758b
Bojan Ramšak <bojan.ramsak@rtvslo.si>
2022-08-19 21:28:55 +0200

    Just skeleton

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+AnyEvent::MQTT::Simple - It's new $module
+
+# SYNOPSIS
+
+    use AnyEvent::MQTT::Simple;
+
+# DESCRIPTION
+
+AnyEvent::MQTT::Simple is ...
+
+# LICENSE
+
+Copyright (C) Bojan Ramšak.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Bojan Ramšak <bojanr@gmx.net>
---
+++ lib/AnyEvent/MQTT/Simple.pm
@@ -0,0 +1,40 @@
+package AnyEvent::MQTT::Simple;
+use 5.010;
+use strict;
+use warnings;
+use AnyEvent::Handle;
+
+our $VERSION = "0.81";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+AnyEvent::MQTT::Simple - It's new $module
+
+=head1 SYNOPSIS
+
+    use AnyEvent::MQTT::Simple;
+
+=head1 DESCRIPTION
+
+AnyEvent::MQTT::Simple is ...
+
+=head1 LICENSE
+
+Copyright (C) Bojan Ramšak.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Bojan Ramšak E<lt>bojanr@gmx.netE<gt>
+
+=cut
+
